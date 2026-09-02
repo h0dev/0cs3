931c2d7402d0af47f048e435934f22ca40cfca92
nuvix CI <actions@users.noreply.github.com>
2026-09-02 13:41:13 +0000

    Publish megaplay provider (built from hung319/nuvix)

--- providers/megaplay.js
+++ providers/megaplay.js
@@ -1,6 +1,6 @@
 /**
  * megaplay - Built from src/megaplay/
- * Generated: 2026-09-02T13:35:38.058Z
+ * Generated: 2026-09-02T13:41:13.244Z
  */
 var __defProp = Object.defineProperty;
 var __defProps = Object.defineProperties;
